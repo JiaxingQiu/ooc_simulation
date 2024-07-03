24054403fe5d6a3ef690403307009108fa2adfd0
JiaxingQiu <jiaxingq1996@gmail.com>
2024-07-03 13:17:52 -0400

    split lr and lm

--- sim_run.R
+++ sim_run.R
@@ -125,19 +125,19 @@ sjob_lm = slurm_map(
 )
 save(sjob_lm, file = "ooc_run_lm.Rdata")
 
-sjob_lr = slurm_map(
-  split(simulation_conditions, simulation_conditions$id),
-  run_wrapper_lr,
-  nodes=nrow(simulation_conditions),
-  cpus_per_node = 1,
-  jobname = "ooc_run_lr",
-  submit = TRUE,
-  preschedule_cores = F,
-  slurm_options =
-    c(account = "netlab", partition = "standard", time = "3-00:00:00"), # standard
-  global_objects = lsf.str()
-)
-save(sjob_lr, file = "ooc_run_lr.Rdata")
+# sjob_lr = slurm_map(
+#   split(simulation_conditions, simulation_conditions$id),
+#   run_wrapper_lr,
+#   nodes=nrow(simulation_conditions),
+#   cpus_per_node = 1,
+#   jobname = "ooc_run_lr",
+#   submit = TRUE,
+#   preschedule_cores = F,
+#   slurm_options =
+#     c(account = "netlab", partition = "standard", time = "3-00:00:00"), # standard
+#   global_objects = lsf.str()
+# )
+# save(sjob_lr, file = "ooc_run_lr.Rdata")
 
 
 
