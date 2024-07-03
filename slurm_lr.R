dd0fadddb02b9300983d57a792c97b117f2dcb8b
JiaxingQiu <jiaxingq1996@gmail.com>
2024-07-03 13:39:53 -0400

    change slurm job cpuper

--- slurm_lr.R
+++ slurm_lr.R
@@ -4,12 +4,12 @@ sjob_lr = slurm_map(
   split(simulation_conditions, simulation_conditions$id),
   run_wrapper_lr,
   nodes=nrow(simulation_conditions),
-  cpus_per_node = 4,
+  cpus_per_node = 1,
   jobname = "ooc_run_lr",
   submit = TRUE,
   preschedule_cores = F,
   slurm_options =
-    c(account = "netlab", partition = "parallel", time = "2-00:00:00"), 
+    c(account = "netlab", partition = "standard", time = "2-00:00:00"), 
   global_objects = lsf.str()
 )
 save(sjob_lr, file = "ooc_run_lr.Rdata")
