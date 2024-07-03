11f598503e8f8cba169c987cb5c023e7faa72af6
JiaxingQiu <jiaxingq1996@gmail.com>
2024-07-03 13:35:20 -0400

    change slurm job cpupernode

--- slurm_lm.R
+++ slurm_lm.R
@@ -4,7 +4,7 @@ sjob_lm = slurm_map(
   split(simulation_conditions, simulation_conditions$id),
   run_wrapper_lm,
   nodes=nrow(simulation_conditions),
-  cpus_per_node = 1,
+  cpus_per_node = 4,
   submit = TRUE,
   preschedule_cores = F,
   slurm_options =
